f15fa4a833a6e16d76d374d52440d1216b52c949
Raytwum <raytwum@gmail.com>
2016-04-20 00:38:48 +0200

    Update asuro.ino
    replace Serial.read() with Serial.readString() to get the Strings.

--- asuro.ino
+++ asuro.ino
@@ -1,7 +1,8 @@
-String incomingByte= "test";   // for incoming serial data
+String incomingString; 
 
 void setup() {
         Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+        pinMode(8,INPUT);
 }
 
 void loop() {
@@ -9,10 +10,10 @@ void loop() {
         // send data only when you receive data:
         if (Serial.available() > 0) {
                 // read the incoming byte:
-                incomingByte = Serial.read();
+                incomingString = Serial.readString();
 
                 // say what you got:
                 Serial.print("I received: ");
-                Serial.println(incomingByte);
+                Serial.println(incomingString+"\n");
         }
 }
