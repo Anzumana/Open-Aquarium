78b3d09f181517d6573c9ffbc5a5f590bd408bec
Anzumana Taal <anzumana.taal@me.com>
2016-04-20 07:44:07 +0200

    update index.js
    Commented codelines
    And put all the functions we tried yesterday into the codebase

--- node_js/index.js
+++ node_js/index.js
@@ -1,6 +1,7 @@
 var five = require("johnny-five");
 
 five.Board().on("ready", function() {
+	// read the current Temperature
   var temperature = new five.Thermometer({
     controller: "TMP36",
     pin: "A0"
@@ -9,5 +10,14 @@ five.Board().on("ready", function() {
   temperature.on("data", function() {
     console.log(this.celsius + "°C", this.fahrenheit + "°F");
   });
+
+	// turn on led on pin 
+	this.pinMode(13, five.Pin.PWM);
+	this.analogWrite(13, 255);
+	// try to send data to pin 1 and receive data on the asuro
+	// todo this worked however the received data was not the send data for some point.
+	// looked like voltage
+	this.pinMode(1, five.Pin.PWM);
+	this.analogWrite(1, 21);
 });
 console.log("Waiting for device to connect");
